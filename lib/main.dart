81ca319ad7a36e9ba87f757609a0d2dd0be960d5
Nurlord <352933nurzat@gmail.com>
2025-04-15 13:06:43 +0500

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audiobooks_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screens/onboarding_screen.dart';
@@ -8,22 +9,23 @@ import 'screens/main_screen.dart';
 import 'screens/forget_password_screen.dart';
 import 'screens/details_screen.dart';
 import 'screens/text_screen.dart';
-import 'screens/splash_screen.dart'; 
+import 'screens/splash_screen.dart';
 import 'constants/theme_constants.dart';
 import 'models/book.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+  await AuthService.init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -39,14 +41,18 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/splash', // Changed initial route to splash screen
       routes: {
-        '/splash': (context) => const SplashScreen(), // Added splash screen route
-        '/onboarding': (context) => const OnboardingScreen(), // Changed from '/' to '/onboarding'
+        '/splash':
+            (context) => const SplashScreen(), // Added splash screen route
+        '/onboarding':
+            (context) =>
+                const OnboardingScreen(), // Changed from '/' to '/onboarding'
         '/register': (context) => const RegisterScreen(),
         '/login': (context) => const LoginScreen(),
         '/personalize': (context) => const PersonalizeScreen(),
         '/main': (context) => const MainScreen(),
         '/forget_password': (context) => const ForgetPasswordScreen(),
-        '/details': (context) => DetailsScreen(
+        '/details':
+            (context) => DetailsScreen(
               book: ModalRoute.of(context)!.settings.arguments as Book,
             ),
         '/text': (context) => const TextScreen(),
